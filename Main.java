0ad13b68c0c53d40748cb55cf93f1d6ff8700773
aadirai31 <94331877+aadirai31@users.noreply.github.com>
2025-10-17 11:23:50 +1100

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Entry point of the program
+        System.out.println("Hello, World!");
+
+        // Add your code here
+    }
+}
